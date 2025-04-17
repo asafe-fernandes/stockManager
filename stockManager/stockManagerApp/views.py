b01c3c2f6c2ec0b84c5fd963a8a34a1fd6cb83ab
asafe fernandes <177295792+asafe-fernandes@users.noreply.github.com>
2025-04-17 16:22:37 -0300

    created item model and created admin

--- stockManager/stockManagerApp/views.py
+++ stockManager/stockManagerApp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from .models import Item
 # Create your views here.
 
 
 def home(request):
     return render(request, "home.html")
+
+
+def items(request):
+    items = Item.objects.all()
+    return render(request, "items.html", {"items": items})
