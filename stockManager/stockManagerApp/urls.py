8697834f902c87e46c2ddc38a0218f56aafb0d42
asafe fernandes <177295792+asafe-fernandes@users.noreply.github.com>
2025-04-18 16:07:46 -0300

    created edit view

--- stockManager/stockManagerApp/urls.py
+++ stockManager/stockManagerApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("items/", views.items, name="itens"),
-    path("items/add", views.createItem, name="createItem")
+    path("items/add", views.createItem, name="createItem"),
+    path("items/edit/<int:id>/", views.editItem, name="editItem"),
 ]
--- stockManager/stockManagerApp/views.py
+++ stockManager/stockManagerApp/views.py
@@ -21,3 +21,15 @@ def createItem(request):
     else:
         form = ItemForm()
     return render(request, "create_item.html", {"form": form})
+
+
+def editItem(request, id):
+    item = Item.objects.get(id=id)
+    if request.method == "POST":
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('itens')
+    else:
+        form = ItemForm(instance=item)
+    return render(request, "edit_item.html", {"form": form, "edit": True})
